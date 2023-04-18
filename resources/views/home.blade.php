35cc5d9c0b70a4e5d22a55ca3551cba685c34e75
Andrew Lee <andrewimg@gmail.com>
2023-04-18 10:49:27 -0700

    working welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="app-title">
         <h1>Poker Tracker</h1>
+        <p>Sign up for a free account <br>
+        to start tracking stats and sessions</p>
     </div>
-
 @endsection
