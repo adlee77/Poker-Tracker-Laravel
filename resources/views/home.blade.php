5528033f2239887faa3952909778cc3e39863d39
Andrew Lee <andrewimg@gmail.com>
2023-04-18 00:06:33 -0700

    homepage styling done for now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    Welcome
-                </div>
-            </div>
-        </div>
+    <div class="app-title">
+        <h1>Poker Tracker</h1>
     </div>
-</div>
+
 @endsection
